3635a2ed33fddd30290b58073df51fdcc5b1e904
Surbhi83 <surabhi.gupta.sai@gmail.com>
2017-01-16 18:13:13 -0500

    Array drills

---
+++ JavaScript/Array/Array Drill2 Adding array items/jsbin.falika.js
@@ -0,0 +1,37 @@
+function addToList(list, item) {
+  // your code goes here
+}
+
+
+
+
+/* From here down, you are not expected to 
+   understand.... for now :)  
+   
+   
+   Nothing to see here!
+   
+*/
+
+
+// tests
+
+function testAddToList() {
+  
+  var input1 = ["red", "blue", "green"]
+  var input2 = "pink"
+  var expected = ["red", "blue", "green", "pink"]
+  var result = addToList(input1, input2);
+  
+  if (
+    result && result.length && expected.length === result.length &&
+    expected.every(function(item) {
+      return result.indexOf(item) > -1;
+    })) {
+    console.log('SUCCESS: `addToList` works!');
+  } else {
+    console.error('FAILURE: `addToList` is not working');
+  } 
+}
+
+testAddToList();
