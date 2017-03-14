4753bbec5a274a7fa09a92cb76556aa90f6f8f03
Surbhi83 <surabhi.gupta.sai@gmail.com>
2017-03-14 19:20:48 -0400

    text analyzer

--- JQuery/text-analyzer-starter-files-master/text-analyzer-starter-files-master/app.js
+++ JQuery/text-analyzer-starter-files-master/text-analyzer-starter-files-master/app.js
@@ -1,22 +1,15 @@
 function textAnalyze(){
+  $('#button1').click(function(){
+    $('dl').removeClass('hidden');
   
-     $('#button1').click(function(){
-    
-  
-   $('dl').removeClass('hidden');
-  
-   var message = $('textarea#user-text').val();
-   // alert("message"+message);
-     var countWords=message.split(" ").length;
-      $('.wcount').append(countWords);
-
-
+      var message = $('textarea#user-text').val().trim();
+      message=message.replace(".","");
+       message=message.replace(",","");
+      var countWords=message.split(" ").length;
+    $('.wcount').text(countWords);
 
       var inputArray= message.split(" ");
-
-
       var outputArray = [];
-    
     for (var i = 0; i < inputArray.length; i++)
     {
         if ((jQuery.inArray(inputArray[i], outputArray)) == -1)
@@ -24,25 +17,14 @@ function textAnalyze(){
             outputArray.push(inputArray[i]);
         }
     }
-    $('.uwcount').append(outputArray.length);
+    $('.uwcount').text(outputArray.length);
 
-var totalCharLength =  message.replace(/\s+/g, '').length;
-  var averageLen= (totalCharLength / countWords);
-alert("total char length is:"+totalCharLength);
-$('.avgWLen').append(averageLen);
-
- var countSentence=message.split(".").length;
- countSentence=countSentence-1;
- alert("message"+countSentence);
- var averageSenLen= (totalCharLength / countSentence);
-      $('.wSen').append(averageSenLen);
-
-
+      var totalCharLength =  message.replace(/\s+/g,'').length;
+      var averageLen= (totalCharLength / countWords);
+            // alert("total char length is:"+totalCharLength);
+    $('.avgWLen').text(averageLen);
+    
     })
- 
-
-
-
 
   }
   textAnalyze();
